9b60c796208dc8464c0ce09612b4f2230ca68b1d
Rebecca Kane <Rebecca Kane>
2017-03-30 16:40:39 +0100

    forgot to save...

--- SleepSounds/SleepSounds/MainPage.xaml.cs
+++ SleepSounds/SleepSounds/MainPage.xaml.cs
@@ -29,168 +29,12 @@ namespace SleepSounds
             this.InitializeComponent();
         }
 
-        // Arrays
-        void PlayMultiple()//this should loop through an array of names, set the media element name and play
-        {
-            MediaElement me = new MediaElement();
-           
-            for(int i = 0; i < playing.Length; i++)
-            {
-
-                //playing stores the media elements names
-                me.Name = playing[i];
-                me.Play();
-            }
-        }
-
-        // Count Variables
-        public int rainC = 0;
-        public int waveC = 0;
-        public int thunC = 0;
-        public int wNoiseC = 0;
-        public int fireC = 0;
-        public int forThuC = 0;
-        public int cityC = 0;
-        public int birdsC = 0;
-
-        //isPlaying Variables
-        public bool rainIP = false;
-        public bool waveIP = false;
-        public bool thunIP = false;
-        public bool whIP = false;
-        public bool fireIP = false;
-        public bool forthunIP = false;
-        public bool cityIP = false;
-        public bool birdsIP = false;
-
-
-    // Button methods
-    private void rainButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (rainC == 0)
-            {
-                medrain.Play();
-                rainC++;
-                rainIP = true;
-                
-            }
-            else
-            {
-                medrain.Stop();
-                rainC--;
-                rainIP = false;
-            }
-        }
-
-        private void wavButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (waveC == 0)
-            {
-                waves.Play();
-                waveC++;
-                waveIP = true;
-
-            }
-            else
-            {
-                waves.Stop();
-                waveC--;
-                waveIP = false;
-            }
-        }
-
-        private void thuButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (thunC == 0)
-            {
-                thunder.Play();
-                thunC++;
-                thunIP = true;
-            }
-            else
-            {
-                thunder.Stop();
-                thunC--;
-                thunIP = false;
-            }
-        }
-
-        private void whnoiButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (wNoiseC == 0)
-            {
-                whitenoise.Play();
-                wNoiseC++;
-                whIP = true;
-            }
-            else
-            {
-                whitenoise.Stop();
-                wNoiseC--;
-                whIP = false;
-            }
-        }
-
-        private void fireButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (fireC == 0)
-            {
-                fire.Play();
-                fireC++;
-                fireIP = true;
-            }
-            else
-            {
-                fire.Stop();
-                fireC--;
-                fireIP = false;
-            }
-        }
-
-        private void cityButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (cityC == 0)
-            {
-                city.Play();
-                cityC++;
-                cityIP = true;
-            }
-            else
-            {
-                city.Stop();
-                cityC--;
-                cityIP = false;
-            }
-        }
-
-        private void birdsButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (birdsC == 0)
-            {
-                birds.Play();
-                birdsC++;
-                birdsIP = true;
-            }
-            else
-            {
-                birds.Stop();
-                birdsC--;
-                birdsIP = false;
-            }
-        }
-
-        private void createCombo_Click(object sender, RoutedEventArgs e)
-        {
-            MediaElement me = new MediaElement();
-            //playing[0] = ;
-        }
-
+        // Sound buttons
         private void SoundButton_Click(object sender, RoutedEventArgs e)
         {
-            Button curr = (Button)sender;
-            // get substring
-            string name = curr.Name.Substring(0, curr.Name.IndexOf("_"));
-            MediaElement me = (MediaElement)FindName(name);
+            Button curr = (Button)sender; // get the click event object (i.e. current sound)
+            string name = curr.Name.Substring(0, curr.Name.IndexOf("_")); // get the name of the click event before the _
+            MediaElement me = (MediaElement)FindName(name); // set media element = 
 
             if (me.Tag.ToString() == "N")
             {
@@ -203,11 +47,12 @@ namespace SleepSounds
                 me.Tag = "N";
             }
 
-        }
+        } // end sound buttons
 
-        private void forRainButton_Click(object sender, RoutedEventArgs e)
+        private void createCombo_Click(object sender, RoutedEventArgs e)
         {
-
+           
         }
+        
     }
 }
